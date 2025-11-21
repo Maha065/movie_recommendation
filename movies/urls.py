23011226db77367d0ad5bc94fe9277d4882b5179
Maha065 <maha060506@gmail.com>
2025-11-21 18:14:05 +0100

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,1 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import MovieViewSet, AuthViewSet
 
+router = DefaultRouter()
+router.register(r'movies', MovieViewSet, basename='movie')
+router.register(r'auth', AuthViewSet, basename='auth')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
