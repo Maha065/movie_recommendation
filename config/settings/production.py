ea22fc153a584a965ab0ddd1e8efa35a375226aa
Maha065 <maha060506@gmail.com>
2025-11-21 18:36:12 +0100

    Create production.py

---
+++ config/settings/production.py
@@ -0,0 +1,30 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': config('REDIS_URL'),
+    }
+}
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_SECURITY_POLICY = {
+    'default-src': ("'self'",),
+}
